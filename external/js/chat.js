11437360036316fe574f1ceaa21a44fa01553053
Retype GitHub Action <retype@object.net>
2026-09-09 12:37:50 +0000

    Refreshes Retype-generated documentation.
    Process triggered by ipmingsum2.

--- external/js/chat.js
+++ external/js/chat.js
@@ -1,4 +1,6 @@
-let validEmail = true; // Let me know if this had caused bugs.
+function validEmail() {
+  return true;
+}
 const SUPABASE_URL = "https://lflkpziiwnoamvtrbcil.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_tFvlhmTEDV3SOSVp0JvVzg_KHXFiDNb";
 const sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
